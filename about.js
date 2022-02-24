d599c3c54644608aeeb291f665a8cb2122955a5f
Tyler Hermansen <ttime40@hotmail.com>
2022-02-23 20:25:25 -0700

    first commit

---
+++ about.js
@@ -0,0 +1,30 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('form submited');
+}
+
+
+let form = document.querySelector('form#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+// function mOver(obj) {
+// 	obj.innerHTML = "Grumpy cat says you are doing good"
+
+// }
+
+var x = document.getElementById("myBtn");
+x.addEventListener("click", myFunction);
+x.addEventListener("click", someOtherFunction);
+
+
+// function mOut(obj) {
+// 	obj.getElementById("photo").addEventListener("mOut(this)", mOut, true);
+// }
+	
+
---
+++ profile.js
@@ -0,0 +1,18 @@
+var x = document.getElementById("color");
+x.addEventListener("click", myColor);
+x.addEventListener("click", myPlace);
+x.addEventListener("click", myRitual);
+
+function myColor() {
+    alert ("My Favorite color is Blue");
+  }
+  
+  function myPlace() {
+    alert ("My favorite Place is anywhere but my job");
+  }
+
+  function myRitual() {
+    alert ("Metitating in the mornings.");
+  }
+  
+ 
